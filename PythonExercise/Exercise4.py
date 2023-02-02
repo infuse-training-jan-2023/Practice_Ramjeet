83c57c6ba0eaba6b05ce6de36acc7ca032f7ada5
Ramjeet <ramjeetfaldessai@gmail.com>
2023-02-02 11:35:18 +0530

    few changes to exercise 4

--- PythonExercise/Exercise4.py
+++ PythonExercise/Exercise4.py
@@ -3,19 +3,22 @@
 
 class exercisefour:
     def sub_arr_ops(self,numlist,k):
-        count=0
-        total= 0
-        dict = {}
-        dict[0] = 1
-        for i in range(len(numlist)):
-            total += numlist[i]
-            if total - k in dict:
-                count += dict[total - k] 
-            if total in dict:
-                dict[total] += 1  
-            else: 
-                dict[total] = 1 
-        return count
+        try:
+            count=0
+            total= 0
+            dict = {}
+            dict[0] = 1
+            for i in range(len(numlist)):
+                total += numlist[i]
+                if total - k in dict:
+                    count += dict[total - k] 
+                if total in dict:
+                    dict[total] += 1  
+                else: 
+                    dict[total] = 1 
+            return count
+        except:
+            return 'error'
 
     
     
